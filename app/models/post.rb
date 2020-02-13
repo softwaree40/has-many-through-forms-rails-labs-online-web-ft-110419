a7d0e212bb75d7afe1a15588fdefadb0d682b644
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-13 05:42:41 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  # accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes[:name].blank? }
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |cat_attr|
+      if cat_attr[:name].present?
+        category = Category.find_or_create_by(cat_attr)
+        self.categories << category
+      end
+    end
+
+  end
 end
